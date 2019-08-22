7c6cd5a039968ce4320a7d2556d6691989054472
Patrick Kim <pkim050@gmail.com>
2019-08-22 01:23:29 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,36 @@
 export default (state = [], action) => {
-  return state;
+  let quoteId
+  let quotes
+  if (action.quoteId) {
+    quoteId = action.quoteId
+  }
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+
+    case "REMOVE_QUOTE":
+      quotes = state.filter(quote => quote.id !== quoteId)
+      return quotes
+
+    case "UPVOTE_QUOTE":
+      quotes = state.map(quote => {
+        if (quote.id === quoteId) {
+          quote.votes++
+        }
+        return quote
+      })
+      return quotes
+
+    case "DOWNVOTE_QUOTE":
+      quotes = state.map(quote => {
+        if (quote.id === quoteId && quote.votes > 0) {
+          quote.votes--
+        }
+        return quote
+      })
+      return quotes
+
+    default:
+      return state
+  }
 }
