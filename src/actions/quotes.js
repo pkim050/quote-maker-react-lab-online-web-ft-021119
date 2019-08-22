2ad8b747de7b44f3f94f2d7d31f71c5df524cca3
Patrick Kim <pkim050@gmail.com>
2019-08-22 01:22:18 +0000

    Automatically backed up by Learn

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -2,15 +2,15 @@
 export const addQuote = (quote) => {
     return {type: "ADD_QUOTE", quote: quote}
  }
- 
+
  export const removeQuote = (quoteId) => {
     return {type: "REMOVE_QUOTE", quoteId: quoteId}
  }
- 
+
  export const upvoteQuote = (quoteId) => {
     return {type: "UPVOTE_QUOTE", quoteId: quoteId}
  }
- 
+
  export const downvoteQuote = (quoteId) => {
     return { type: "DOWNVOTE_QUOTE", quoteId: quoteId }
  }
